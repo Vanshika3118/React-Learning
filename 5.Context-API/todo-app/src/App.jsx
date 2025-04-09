1d524c827cba2aae535618d28348936d4d61e866
Vanshika <vanshika1455.be22@chitkarauniversity.edu.in>
2025-04-09 19:09:12 +0530

    useReducer hook

--- 5.Context-API/todo-app/src/App.jsx
+++ 5.Context-API/todo-app/src/App.jsx
@@ -2,14 +2,35 @@ import AppName from "./components/AppName";
 import AddTodo from "./components/AddTodo";
 import "./App.css";
 import TodoItems from "./components/TodoItems";
-import { useState } from "react";
+// import { useState, useReducer } from "react";
 import WelcomeMessage from "./components/WelcomeMessage";
-import { TodoItemsContext } from "./store/todo-items-store";
+import TodoItemsContextProvider from "./store/todo-items-store";
+
+// In React, a pure function is a function whose output solely depends on its input arguments, without causing any side effects. This means that given the same input, a pure function will always return the same output, and it does not modify any external variables or states. React assumes that every component written is a pure function.
+
+// const todoItemsReducer = (currTodoItems, action) => {
+//   // pure function that take action onject and return current state , we can make this function in another file
+
+//   let newTodoItems = currTodoItems;
+//   if (action.type === "ADD_ITEM") {
+//     newTodoItems = [
+//       ...currTodoItems,
+//       {
+//         name: action.payload.itemName,
+//         date: action.payload.itemDueDate,
+//       },
+//     ];
+//   } else if (action.type === "DELETE_ITEM") {
+//     newTodoItems = currTodoItems.filter((item) => item.name !== action.payload.itemName);
+//   }
+//   return newTodoItems;
+// };
 
 function App() {
-  const [todoItems, setTodoItems] = useState([]);
+  //const [todoItems, setTodoItems] = useState([]);
+  // const [todoItems, dispatchTodoItems] = useReducer(todoItemsReducer, []);
 
-  const addNewItem = (itemName, itemDueDate) => {
+  // const addNewItem = (itemName, itemDueDate) => {
     // const newTodoItems = [
     //   ...todoItems,
     //   {
@@ -18,32 +39,46 @@ function App() {
     //   },
     // ];
     // setTodoItems(newTodoItems);
-    setTodoItems((currValue) => [
-      ...currValue,
-      { name: itemName, date: itemDueDate },
-    ]);
-  };
-  const deleteItem = (itemName) => {
-    const newTodoItems = todoItems.filter((item) => item.name !== itemName);
-    setTodoItems(newTodoItems);
-  };
+
+    // setTodoItems((currValue) => [
+    //   ...currValue,
+    //   { name: itemName, date: itemDueDate },
+    // ]);
+
+  //   const newItemAction = {
+  //     type: "ADD_ITEM",
+  //     payload: {
+  //       itemName,
+  //       itemDueDate,
+  //     },
+  //   };
+
+  //   dispatchTodoItems(newItemAction); // dispatching action to reducer function
+  // };
+  // const deleteItem = (itemName) => {
+  //   // const newTodoItems = todoItems.filter((item) => item.name !== itemName);
+  //   // setTodoItems(newTodoItems);
+
+  //   const deleteItemAction = {
+  //     type: "DELETE_ITEM",
+  //     payload: {
+  //       itemName,
+  //     },
+  //   };
+  //   dispatchTodoItems(deleteItemAction); // dispatching action to reducer function
+  // };
 
   return (
-    <TodoItemsContext.Provider
-      value={{
-        todoItems,
-        addNewItem,
-        deleteItem,
-      }}
-    >
+    <TodoItemsContextProvider>
       <center className="todo-container">
         <AppName />
         <AddTodo />
         <WelcomeMessage />
         <TodoItems />
       </center>
-    </TodoItemsContext.Provider>
+      </TodoItemsContextProvider>
   );
 }
+
 
 export default App;
--- 5.Context-API/todo-app/src/store/todo-items-store.jsx
+++ 5.Context-API/todo-app/src/store/todo-items-store.jsx
@@ -1,4 +1,56 @@
-import { createContext } from "react"
-
+import { createContext } from "react";
+import { useReducer } from "react";
 
 export const TodoItemsContext = createContext([]);
+
+const todoItemsReducer = (currTodoItems, action) => {
+  // pure function that take action onject and return current state , we can make this function in another file
+
+  let newTodoItems = currTodoItems;
+  if (action.type === "ADD_ITEM") {
+    newTodoItems = [
+      ...currTodoItems,
+      {
+        name: action.payload.itemName,
+        date: action.payload.itemDueDate,
+      },
+    ];
+  } else if (action.type === "DELETE_ITEM") {
+    newTodoItems = currTodoItems.filter((item) => item.name !== action.payload.itemName);
+  }
+  return newTodoItems;
+};
+
+const TodoItemsContextProvider = ({ children }) => {
+  const [todoItems, dispatchTodoItems] = useReducer(todoItemsReducer, []);
+  const addNewItem = (itemName, itemDueDate) => {
+    const newItemAction = {
+      type: "ADD_ITEM",
+      payload: {
+        itemName,
+        itemDueDate,
+      },
+    };
+
+    dispatchTodoItems(newItemAction); // dispatching action to reducer function
+  };
+  const deleteItem = (itemName) => {
+    // const newTodoItems = todoItems.filter((item) => item.name !== itemName);
+    // setTodoItems(newTodoItems);
+
+    const deleteItemAction = {
+      type: "DELETE_ITEM",
+      payload: {
+        itemName,
+      },
+    };
+    dispatchTodoItems(deleteItemAction); // dispatching action to reducer function
+  };
+  return (
+    <TodoItemsContext.Provider value={{ todoItems, addNewItem, deleteItem }}>
+      {children}
+    </TodoItemsContext.Provider>
+  );
+};
+
+export default TodoItemsContextProvider;
